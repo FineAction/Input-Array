4007f941e528a4844776e9340f48380d50a31239
FineAction <deleriour1@gmail.com>
2025-04-05 11:20:20 +0100

    Update Program.cs
    Fixed misspelt strings.

--- Program.cs
+++ Program.cs
@@ -9,11 +9,11 @@
 
         Console.WriteLine ("Specify Parameters ( this will Parameter[0] )");
         Consoleinput = Console.ReadLine(); input0 = Consoleinput;
-        Console.WriteLine ($"Parameter[1]: {input0} "); Console.Clear();
+        Console.WriteLine ($"Parameter[0]: {input0} "); Console.Clear();
 
         Console.WriteLine ("Please enter Parameter[1]");
         Consoleinput = Console.ReadLine(); input1 = Consoleinput;
-        Console.WriteLine ($"Parameter[2]: {input1} "); Console.Clear();
+        Console.WriteLine ($"Parameter[1]: {input1} "); Console.Clear();
 
         string[] Parameters = { input0, input1 };
         Console.WriteLine($"Parameter Array built. P1 {Parameters[0]} : P2 {Parameters[1]} ");
